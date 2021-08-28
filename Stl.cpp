3e5e0c349b358d39bc956e1378a0dca5c1aa6fb1
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2021-08-28 16:45:17 +0530

    Update Stl.cpp

--- Stl.cpp
+++ Stl.cpp
@@ -1,1 +1,28 @@
 //Algorithm
+//Sorting Vector
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+		vector<int> arr;
+		for(int i=5;i>0;i--){
+			arr.push_back(i);
+		}
+		for(auto x:arr){
+			cout<<x<<" ";
+		}
+		cout<<endl;
+		sort(arr.begin(),arr.end());
+		for(int i=0;i<5;i++){
+			cout<<arr[i]<<" ";
+		}
+		cout<<endl;
+		////sorting in the descending order
+		sort(arr.begin(),arr.end(),greater<int>());
+		for(auto x:arr){
+			cout<<x<<" ";
+		}
+		
+		
+}
