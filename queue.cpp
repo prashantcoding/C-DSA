c59ce09903f5fd77a5598aeeb45d5162593cfc90
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2021-08-31 12:45:31 +0530

    Update queue.cpp
    stack using queues

--- queue.cpp
+++ queue.cpp
@@ -64,3 +64,71 @@ int main(){
 	qu.pop();
 	qu.pop();
 }
+////////////////////stack using queues////////////////////////////////
+#include<iostream>
+#define max 100
+using namespace std;
+class queue{
+	public:
+	int arr[max];
+	int front;
+	int rear;
+	public:
+	queue(){
+	 front=-1;
+	 rear=-1;
+	}
+	bool isfull();
+	bool isempty();
+	void push(int x);
+	int pop();
+	
+};
+bool queue::isempty(){
+	if(front==-1||front>rear)
+		return true;
+	return false;
+}
+bool queue::isfull(){
+	if(rear==max)
+		return true;
+	return false;
+}
+void queue::push(int x){
+	if(isfull()==true){
+		cout<<"queue Overflow"<<endl;
+		return;
+	}
+	if(front==-1){
+		arr[++front]=x;
+		rear++;
+		return;
+	}
+	arr[++rear]=x;
+	
+	
+}
+int queue::pop(){
+	if(isempty()==true){
+		cout<<"queue Underflow"<<endl;
+	}
+	int x=arr[rear--];
+	cout<<"poppped out "<<x<<endl;
+	return 0;
+}
+int main(){
+	queue qu;
+	qu.push(5);
+	qu.push(15);
+	qu.push(20);
+	qu.pop();
+	if(qu.isempty()==true){
+		cout<<"queue empty"<<endl;
+	}
+	else{
+		cout<<"queue is not empty"<<endl;
+	}
+	qu.pop();
+	qu.pop();
+
+}
