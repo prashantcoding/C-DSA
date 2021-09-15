821cb4d56f8b82f9aa70ddab4dfb11ad04fe32e2
Prashant Goswami <87839549+prashantcoding@users.noreply.github.com>
2021-09-15 17:39:02 +0530

    Update Tree-Base.cpp

--- Tree/Tree-Base.cpp
+++ Tree/Tree-Base.cpp
@@ -1,23 +1,52 @@
-//Trees 
-#include <bits/stdc++.h>
+//structure of a tree
+#include<iostream>
 using namespace std;
-struct	Node{
-	int data;
-	struct Node	*left;
-	struct Node *right;
-	Node(int val){
-		data=val;
-		left=NULL;
-		right=NULL;
-	}
+
+class Node{
+	public:
+		int data;
+		Node *right;
+		Node *left;
+		Node(int val){
+			this->data=val;
+			right=NULL;
+			left=NULL;
+		}
 	
 };
-int main(){
-	struct Node*root=new Node(1);
-	root->left=new Node(2);
-	root->right=new Node(3);
+void inorder(Node *root){
+	if(root==NULL) return ;
+	inorder(root->left);
+	cout<<root->data<<" ";
+	inorder(root->right);
+	
 
-root->left->left=new Node(4);
-	return 0;
 }
-
+void preorder(Node *root){
+	if(root==NULL) return;
+	cout<<root->data<<" ";
+	preorder(root->left);
+	preorder(root->right);
+}
+void postorder(Node *root){
+	if(root==NULL) return;
+	postorder(root->left);
+	postorder(root->right);
+	cout<<root->data<<" ";
+	
+}
+void tree_inversion(Node *root){
+	
+}
+int main(){
+	Node *root=new Node(5);
+	Node *second=new Node(10);
+	Node *third=new Node(15);
+	root->left=second;
+	root->right=third;
+	inorder(root);
+	cout<<endl;
+	preorder(root);
+	cout<<endl;
+	postorder(root);
+}
